2108207432d6c3caf3bb98370e1a780a09190c70
leoromero97 <leonardo.g.romero.dev@gmail.com>
2024-10-21 18:23:47 -0300

    Seniority component - Create Seniority component with logic and styles - Upgrade types and page but new fileds - Add acai color in tailwind.config.ts

--- src/app/types.ts
+++ src/app/types.ts
@@ -4,6 +4,8 @@ export type DataPropTypes = {
   client?: string;
   rol?: string;
   imageUrl?: string;
+  seniority?: string;
+  discipline?: string;
 };
 
 export type NotionNameType = {
@@ -26,27 +28,41 @@ export type NotionRolType = {
 
 export type NotionClientType = {
   rich_text: {
-    type?: string
-    plain_text?: string
-    href?: string | null
-  }[]
-}
+    type?: string;
+    plain_text?: string;
+    href?: string | null;
+  }[];
+};
 
 export type NotionImageType = {
   files: {
-    id?: string
+    id?: string;
     file: {
-      url?: string
-      expiry_time?: string
-    }
-  }[]
-}
+      url?: string;
+      expiry_time?: string;
+    };
+  }[];
+};
+
+export type NotionDisciplineType = {
+  select: {
+    name?: string;
+  };
+};
+
+export type NotionSeniorityType = {
+  select: {
+    name?: string;
+  };
+};
 
 export type PageDataPropTypes = {
   properties?: {
     Nombre: NotionNameType;
     Rol: NotionRolType;
-    Cliente: NotionClientType
-    Image: NotionImageType
+    Cliente: NotionClientType;
+    Image: NotionImageType;
+    Seniority: NotionSeniorityType;
+    Disciplina: NotionDisciplineType;
   };
 };
